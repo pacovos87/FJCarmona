6767d5b1bb017552dc94c0b6783ad20a66b3082f
fjcarmona <fjcarmona@106-15.info2>
2012-05-21 13:04:50 +0200

    Creacion Nuevo

---
+++ src/FJCarmona/calse1.java
@@ -0,0 +1,14 @@
+package FJCarmona;
+
+public class calse1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		System.out.println("Soy la clase 1");
+
+	}
+
+}
---
+++ src/FJCarmona/clase2.java
@@ -0,0 +1,13 @@
+package FJCarmona;
+
+public class clase2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		System.out.println("Soy la clase 2");
+	}
+
+}
