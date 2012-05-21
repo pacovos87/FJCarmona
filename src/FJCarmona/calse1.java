3782affad430ed8d81790c959ac62e94b6b29c47
fjcarmona <fjcarmona@106-15.info2>
2012-05-21 13:20:02 +0200

    Metodo_Rama1_Clase1

--- src/FJCarmona/calse1.java
+++ src/FJCarmona/calse1.java
@@ -10,8 +10,11 @@ public class calse1 {
 		System.out.println("Soy la clase 1");
 
 	}
-	public static void metodo1_clase2(){
-			
+	public static void metodo1_clase1(){
+		
+	}
+	public static void metodo_rama1_clase1(){
+		
 	}
 
 }
