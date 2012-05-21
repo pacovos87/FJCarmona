93239f1a0aea9b22597be4027ec94789cc9f2a0d
fjcarmona <fjcarmona@106-15.info2>
2012-05-21 13:25:07 +0200

    Creacion Metodo_Rama_Master_Clase2

--- src/FJCarmona/clase2.java
+++ src/FJCarmona/clase2.java
@@ -10,7 +10,10 @@ public class clase2 {
 		System.out.println("Soy la clase 2");
 	}
 	
-	public static void metodo2_clase2(){
+	public static void metodo1_clase2(){
+		
+	}
+	public static void metodo_rama_master_clase2(){
 		
 	}
 }
